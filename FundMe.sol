f5c59d037815bf471de09389fb8a66bda2ff6b75
NicolasTfile <nicolastfile@gmail.com>
2025-05-24 13:14:05 +0100

    Added the fund function into the contract

--- FundMe.sol
+++ FundMe.sol
@@ -1,11 +1,14 @@
-// Get funds from users
-// Withdraw funds
-// Set a minimum funding value in USD
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
 contract FundMe {
-    
+    // Get funds from users
+    // Withdraw funds
+    // Set a minimum funding value in USD
+    function fund() public {
+        
+    }
+
+    // function withdraw() public {}
 }
