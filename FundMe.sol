e6094a9688057b4dcac31a0c012e2e35b06043ae
NicolasTfile <nicolastfile@gmail.com>
2025-05-26 12:57:15 +0100

    Added  minimum usd value

--- FundMe.sol
+++ FundMe.sol
@@ -6,8 +6,11 @@ contract FundMe {
     // Get funds from users
     // Withdraw funds
     // Set a minimum funding value in USD
+
+    uint256 public minimumUsd = 5;
+
     function fund() public payable {
-        require(msg.value > 1e18, "Didn't send enough ETH"); // 1e18 is 1 * 10 ** 18
+        require(msg.value >= minimumUsd, "Didn't send enough ETH"); // 1e18 is 1 * 10 ** 18
     }
 
     // function withdraw() public {}
