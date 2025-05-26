94f3037f1461ea34900cc76d02dc7ccff7e6d806
NicolasTfile <nicolastfile@gmail.com>
2025-05-26 18:22:40 +0100

    Initialized new functions

--- FundMe.sol
+++ FundMe.sol
@@ -10,8 +10,21 @@ contract FundMe {
     uint256 public minimumUsd = 5;
 
     function fund() public payable {
+        
         require(msg.value >= minimumUsd, "Didn't send enough ETH"); // 1e18 is 1 * 10 ** 18
+    
     }
 
     // function withdraw() public {}
+
+    function getPrice() public {
+
+        
+
+    }
+
+    function getConversionRate() public {
+
+    }
+
 }
