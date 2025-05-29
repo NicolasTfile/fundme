cae7758ac2cd690814325b8e34bba19010ec77c5
NicolasTfile <nicolastfile@gmail.com>
2025-05-29 18:44:01 +0100

    Created my first library in solidity.

--- FundMe.sol
+++ FundMe.sol
@@ -2,12 +2,11 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
-    // Get funds from users
-    // Withdraw funds
-    // Set a minimum funding value in USD
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 5e18;
 
@@ -16,7 +15,7 @@ contract FundMe {
 
     function fund() public payable {
         
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH"); // 1e18 is 1 * 10 ** 18
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH"); // 1e18 is 1 * 10 ** 18
 
         funders.push(msg.sender);
 
@@ -25,30 +24,5 @@ contract FundMe {
     }
 
     // function withdraw() public {}
-
-    function getPrice() public view returns (uint256) {
-
-        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // Price of ETH in terms of USD
-        // Solidity doesn't work with decimals therefore you get e.g 200000000000
-        // = 2000.00000000
-        
-        return uint256(price * 1e10); // typecast int to uint since msg.value is a uint
-
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-
-        uint256 ethPrice = getPrice();
-
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1e18 * 1e18 = 1e36 / 1e18 = 1e18
-
-        return ethAmountInUsd;
-
-    }
 
 }
---
+++ PriceConverter.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    
+    function getPrice() internal view returns (uint256) {
+
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        // Solidity doesn't work with decimals therefore you get e.g 200000000000
+        // = 2000.00000000
+        
+        return uint256(price * 1e10); // typecast int to uint since msg.value is a uint
+
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+
+        uint256 ethPrice = getPrice();
+
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1e18 * 1e18 = 1e36 / 1e18 = 1e18
+
+        return ethAmountInUsd;
+
+    }
+
+    function getVersion() internal view returns (uint256) {
+        
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    
+    }
+
+}
