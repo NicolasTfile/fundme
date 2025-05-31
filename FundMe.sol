3bebc4cc4d54cf74b0ba7f5e2112472d0d8970aa
NicolasTfile <nicolastfile@gmail.com>
2025-05-31 17:24:16 +0100

    Withdrawing the funds

--- FundMe.sol
+++ FundMe.sol
@@ -7,22 +7,41 @@ import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
     using PriceConverter for uint256;
-
     uint256 public minimumUsd = 5e18;
-
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    function fund() public payable {
-        
+    function fund() public payable { 
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH"); // 1e18 is 1 * 10 ** 18
-
         funders.push(msg.sender);
-
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-    
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw() public {}
+    function withdraw() public {
+        // for(/*starting index, ending index, step amount*/)
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        // actually withdrawing the funds
+
+        // 1. transfer
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+        
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // 2. send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // 3. call
+        // (bool callSuccess, bytes memory dataReturned)
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
 
 }
